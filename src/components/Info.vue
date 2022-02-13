37573f3345de5a5f99251337d1c15863205a15a6
luis.freire3 <luis.freire@academico.ifpb.edu.br>
2022-02-13 19:04:51 -0300

    Aula de Diretivas

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
 
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <ul>
       <li>Javascript</li>
       <li>PHP</li>
       <li>Python</li>
     </ul>
+    <p v-if="1<2">1 É MENOR Q 2</p>
+    <p v-show="mostrar_email"> O email é: {{ email }} </p>
 </div>
 </template>
 
 <script >
  export default {
-   name: "Info"
+   name: "Info",
+   data() {
+     return {
+       esta_trabalhando: false,
+       mostrar_email: true,
+       email: "123@gmail.com"
+     }
+   }
  }
 </script>
