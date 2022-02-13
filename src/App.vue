d8ba47735641a20ff7a6c02c5635131c95093e60
luis.freire3 <luis.freire@academico.ifpb.edu.br>
2022-02-13 14:24:12 -0300

    first Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
-</template>
+<div>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  <HelloWorld />
+  <LifeCycle />
+</div>
+  
+  </template>
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import LifeCycle from './components/LifeCycle.vue'
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      LifeCycle
+    }
+  }
+</script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1> Meu Nome é: {{ nome }}</h1>  
+</template>
+
+
+<script>
+
+export default {
+  name:"LifeCycle",
+  data(){
+    return {
+      nome: "Ainda n sei"
+    }
+  },
+  created(){
+    setTimeout(()=>{
+      this.nome = "Matheus"
+    },1000);
+  },
+  mounted(){
+      setTimeout(()=>{
+      this.nome = "Henrique"
+    },2000);
+  }
+}
+</script>
