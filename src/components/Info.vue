ff220467353c8edb592b48c020c587538fa2eea3
luis.freire3 <luis.freire@academico.ifpb.edu.br>
2022-02-17 15:21:57 -0300

    Aula Renderização de Listas

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,10 +3,11 @@
 
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
     <p v-else>Estou em busca de novas oportunidades</p>
+    <ul >
+      <li v-for="(tech, index) in backend_technologies" v-bind:key="index"> {{ tech }} </li>
+    </ul>
     <ul>
-      <li>Javascript</li>
-      <li>PHP</li>
-      <li>Python</li>
+      <li v-for="(tech, id) in frontend_technologies" v-bind:key="id"> {{ tech.nome }}</li>
     </ul>
     <p v-if="1<2">1 É MENOR Q 2</p>
   <div>
@@ -25,7 +26,19 @@
        esta_trabalhando: false,
        mostrar_email: false,
        email: "123@gmail.com",
-       meu_link: "https://google.com"
+       meu_link: "https://google.com",
+       backend_technologies:["Javascript","PHP","Python"],
+       frontend_technologies:[
+         {
+           id:1,
+           nome:"HTML"
+       },{
+          id:2,
+          nome:"CSS"
+       },{
+          id:3,
+          nome:"Javascript"
+       }]
      }
    },
    methods:{
