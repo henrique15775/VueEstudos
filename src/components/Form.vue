62915da3f386542163e479d360f37fb9525f31ee
luis.freire3 <luis.freire@academico.ifpb.edu.br>
2022-02-17 16:31:39 -0300

    Aula de Eventos

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,38 @@
 <template>
-  <form>
-    <input type="text" >
-    <input type="text" >
-    <input type="Submit" value="Enviar" >
+  <form @submit="enviarFomulario($event)">
+   <div>
+     <input type="text" v-model="nome" >
+   </div>
+   <div>
+     <input type="text" v-model="email" >
+   </div>
+    <Submit />
   </form>
 </template>
 
 
 <script>
+  import InputText from './form/InputText.vue'
+  import Submit from './form/Submit.vue'
   export default{
-    name:"Form"
+    name:"Form",
+    components:{
+      InputText,
+      Submit
+    },data(){
+      return {
+        nome: "",
+        email:""
+      }
+    },
+    methods:{
+      enviarFomulario(e){
+        e.preventDefault()
+        const name = this.nome;
+        const email = this.email;
+
+        window.alert(`Nome = ${name}\n Email = ${email}`)
+      }
+    }
   }
 </script>
