5cd8a7a04b011f667b5bc87a66d8c6d48f059f7b
luis.freire3 <luis.freire@academico.ifpb.edu.br>
2022-02-18 15:46:16 -0300

    Aula do $emit

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,15 +1,24 @@
 <template>
   <img :src="avatar" :alt="descricao">
+  <MudarImagem @mudar-Imagem="mudarImagem" />
 </template>
 
 
 <script>
+ import MudarImagem from './MudarImagem.vue'
 export default{
   name: "Picture",
   data(){
     return{
       avatar: "/favicon.ico",
       descricao: "Henrique"
+    }
+  },components: {
+    MudarImagem
+  },
+  methods:{
+    trocarImagem(){
+
     }
   }
 }
